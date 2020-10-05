1028ceb50452688127db74e6c22d5da8b49a4840
Student <student@uc3m.local>
2020-10-05 16:01:07 +0200

    parte servidor eth día 5/10/2020

--- ipv4_server.c
+++ ipv4_server.c
@@ -38,6 +38,40 @@ int main ( int argc, char * argv[]){
 
 
 
+    while(1) {
+        /* Recibir trama Ethernet del Cliente */
+        unsigned char buffer[ETH_MTU];
+        mac_addr_t src_addr;
+        long int timeout = -1;
+
+        printf("Escuchando tramas Ethernet (tipo=0x%04x) ...\n", eth_type);
+    
+        int payload_len = eth_recv(eth_iface, src_addr, eth_type, buffer, 
+			       ETH_MTU, timeout);
+        if (payload_len == -1) {
+            fprintf(stderr, "%s: ERROR en eth_recv()\n", myself);
+            exit(-1);
+        }
+
+        char src_addr_str[MAC_STR_LENGTH];
+        mac_addr_str(src_addr, src_addr_str);
+    
+        printf("Recibidos %d bytes del Cliente Ethernet (%s):\n", payload_len, src_addr_str);
+        print_pkt(buffer, payload_len, 0);
+
+        /* Enviar la misma trama Ethernet de vuelta al Cliente */
+        printf("Enviando %d bytes al Cliente Ethernet (%s):\n", payload_len, src_addr_str);
+        print_pkt(buffer, payload_len, 0);
+
+        int len = eth_send(eth_iface, src_addr, eth_type, buffer, payload_len);
+        if (len == -1) {
+            fprintf(stderr, "%s: ERROR en eth_send()\n", myself);
+        }
+  }
+
+
+
+
 
 
 
